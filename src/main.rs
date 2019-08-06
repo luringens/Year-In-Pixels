152b25d99c834e070609e4d28a8d7b6227201c3f
Stian Soltvedt <stian@soltvedt.info>
2019-08-06 22:59:17 +0200

    Make pretty random picture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<std::error::Error>> {
+    // Build the CSV reader and iterate over each record.
+    let mut rdr = csv::Reader::from_reader(std::io::stdin());
+    for result in rdr.records() {
+        // The iterator yields Result<StringRecord, Error>, so we check the
+        // error here.
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    let h = 10;
+    let w = 10;
+    let mut buf: Vec<u8> = Vec::with_capacity(w * h * 3);
+    let mut r: u32 = 7;
+    for _ in 0..(h * w * 3) {
+        buf.push(r as u8);
+        r = (r * 3) % 255;
+    }
+
+    let writer = std::fs::File::create("image.png")?;
+    let encoder = image::png::PNGEncoder::new(writer);
+    encoder.encode(&buf, h as u32, w as u32, image::ColorType::RGB(8))?;
+    Ok(())
 }
